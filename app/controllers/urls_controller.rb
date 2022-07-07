c960a5549c4de2c0adaa9d1780eda662e12cb92f
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-07 18:05:18 +0800

    add variable @domain for the current domain name

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,24 +1,21 @@
 class UrlsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_url, only: :show
-  before_action :set_own_url, only: [:destroy]
+  before_action :set_own_url, only: :destroy
 
 
   def index
     @urls = Url.all.includes(:user)
+    @domain = request.base_url
   end
 
   def new
     @url = Url.new
-
   end
 
   def create
     @url = Url.new(url_params)
     @url.user = current_user
-
-    @short = SecureRandom.base64(6)
-    create if Url.exists?(short_url: @short)
 
     @url.short_url= @short
     if @url.save
@@ -41,7 +38,7 @@ class UrlsController < ApplicationController
   def set_own_url
     @url= current_user.urls.find_by_id(params[:id])
     if @url.nil?
-      flash[:alert] = 'this post is either does not belong to you or does not exist'
+      flash[:alert] = 'this url is either does not belong to you or does not exist'
       redirect_to urls_path
     end
   end
