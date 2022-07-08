e032ac48395e2b5a91194121ebb1cef302d11581
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-08 09:56:07 +0800

    add routes with parameter for redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "short_url" => "urls#index"
+  get '/urls' => 'urls#index'
+  get '/:short_url' => 'urls#redirect'
   root :to => "urls#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :urls
+
 end
 
