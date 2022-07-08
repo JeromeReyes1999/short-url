7182794cc2542ba83c1b2ae3bc704ccd0b67a9dc
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-08 18:14:12 +0800

    change long_url's datatype from text to string

--- db/migrate/20220706065426_create_urls.rb
+++ db/migrate/20220706065426_create_urls.rb
@@ -2,7 +2,7 @@ class CreateUrls < ActiveRecord::Migration[6.1]
   def change
     create_table :urls do |t|
       t.string :description
-      t.text :long_url
+      t.string :long_url
       t.string :short_url
       t.integer :count, default:0
       t.belongs_to :user
