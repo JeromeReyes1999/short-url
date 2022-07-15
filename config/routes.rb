7c3913c72b2c0c9fac5cec9e37c8d8988d54e45f
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-15 16:23:04 +0800

    create routes for api and simplify it

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   resources :urls
 
+  namespace :api do
+    resources :regions, :provinces,:districts,:city_municipalities,:barangays, only: :index, defaults: {format: :json}
+  end
+
   get "*path" => "urls#content_not_found" #404
 end
 
